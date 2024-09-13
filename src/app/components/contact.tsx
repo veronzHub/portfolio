b0b08cc2d8fb0694c0e15eb27690020b08d8cb96
veronzHub <veronica.coulombe@gmail.com>
2024-09-13 22:20:07 +0000

    added functional form

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -2,6 +2,7 @@
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
 import Image from "next/image";
+import ContactForm from "./form"; // Import your form component
 
 export default function Contact() {
   const contentRef = useRef(null);
@@ -34,39 +35,28 @@ export default function Contact() {
     <section id="contact" className="contact-section overflow-x-hidden">
       <div className="content container overflow-hidden" ref={contentRef}>
         <motion.div
-          className="flex justify-center items-center md:flex-row flex-col"
+          className="flex md:flex-row flex-col h-full"
           initial="hidden"
           animate={isInView ? "visible" : "hidden"}
           variants={contentVariants}
         >
           <motion.div
-            className="w-full md:w-1/2 gap-10 z-10 overflow-auto mb-20"
+            className="w-full md:w-1/2 gap-10 z-10 overflow-auto pb-10"
             variants={itemVariants}
           >
             <h3 className="pt-10 pb-12 text-6xl text-center text-white">
               Contact Me
             </h3>
             <div className="bg-[rgba(255,255,255,0.8)] text-black p-10 rounded-md">
-              <input
-                type="text"
-                placeholder="Your Name"
-                className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full mb-5"
-              />
-
-              <textarea
-                placeholder="Your Message"
-                className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 h-32 w-full mb-5"
-              />
-              <button className="bg-primary text-white p-3 rounded-md w-full transition-transform duration-200 ease-in-out hover:brightness-125 hover:scale-105 active:scale-95">
-                Send
-              </button>
+              <ContactForm />
             </div>
           </motion.div>
+
           <motion.div
-            className="w-full md:w-1/2 flex justify-end pr-1"
+            className="w-full md:w-1/2 flex justify-end items-end pr-1"
             variants={itemVariants}
           >
-            <div className="relative md:-mr-10">
+            <div className="relative md:-mr-10 self-end">
               <Image
                 src="/contact-g-4.png"
                 width={778}
---
+++ src/app/components/form.tsx
@@ -0,0 +1,145 @@
+import { useState } from "react";
+import { FormikHelpers, Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
+import { createClient } from "@/app/utils/supabase/client";
+
+interface FormValues {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  message: Yup.string().required("Message is required"),
+});
+
+export default function ContactForm() {
+  const supabase = createClient();
+  const [captchaToken, setCaptchaToken] = useState<string | null>(null);
+  const [submitted, setSubmitted] = useState(false);
+  const [captchaError, setCaptchaError] = useState(false);
+
+  const handleSubmit = async (
+    values: FormValues,
+    { setSubmitting }: FormikHelpers<FormValues>
+  ) => {
+    setCaptchaError(false);
+
+    if (!captchaToken) {
+      setCaptchaError(true);
+      setSubmitting(false);
+      return;
+    }
+
+    const { error } = await supabase.from("contact_form").insert([values]);
+
+    if (error) {
+      console.error("Error inserting data:", error);
+    } else {
+      setSubmitted(true);
+    }
+
+    setSubmitting(false);
+  };
+
+  return (
+    <>
+      {submitted ? (
+        <>
+          <p className="text-center text-accent">
+            Thank you for your message!{" "}
+          </p>
+          <p className="text-center text-accent mt-5">
+            I&apos;ll get back to you soon.
+          </p>
+        </>
+      ) : (
+        <Formik
+          initialValues={{ name: "", email: "", message: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting, errors, touched }) => (
+            <Form>
+              <div className="mb-5">
+                <Field
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  className={`border rounded-lg p-2 w-full ${
+                    errors.name && touched.name
+                      ? "border-red-500"
+                      : "border-gray-300"
+                  } focus:outline-none focus:ring-2 focus:ring-secondary`}
+                />
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className="text-red-500 text-xs pt-1"
+                />
+              </div>
+              <div className="mb-5">
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className={`border rounded-lg p-2 w-full ${
+                    errors.email && touched.email
+                      ? "border-red-500"
+                      : "border-gray-300"
+                  } focus:outline-none focus:ring-2 focus:ring-secondary`}
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-xs pt-1"
+                />
+              </div>
+              <div className="mb-5">
+                <Field
+                  as="textarea"
+                  name="message"
+                  placeholder="Message"
+                  className={`border rounded-lg p-2 w-full h-32 ${
+                    errors.message && touched.message
+                      ? "border-red-500"
+                      : "border-gray-300"
+                  } focus:outline-none focus:ring-2 focus:ring-secondary`}
+                />
+                <ErrorMessage
+                  name="message"
+                  component="div"
+                  className="text-red-500 text-xs"
+                />
+              </div>
+
+              <HCaptcha
+                sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY as string}
+                onVerify={(token: string) => {
+                  setCaptchaToken(token);
+                  setCaptchaError(false);
+                }}
+              />
+              {captchaError && (
+                <div className="text-red-500 text-xs pt-1">
+                  Please complete the CAPTCHA.
+                </div>
+              )}
+
+              <button
+                type="submit"
+                className="bg-primary text-white p-3 rounded-md w-full mt-5"
+                disabled={isSubmitting}
+              >
+                Send
+              </button>
+            </Form>
+          )}
+        </Formik>
+      )}
+    </>
+  );
+}
