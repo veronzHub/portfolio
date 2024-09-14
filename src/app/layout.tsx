0b7c51d5cb1dcc57e4f1338936a6743ff6e65a9f
veronzHub <veronica.coulombe@gmail.com>
2024-09-14 19:08:35 +0000

    added more font versions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/qgl6dif.css" />
+      </head>
       <body className="base-100">{children}</body>
     </html>
   );
