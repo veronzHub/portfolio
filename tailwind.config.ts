f25d36cdcd7bf23fb90b87a2d372f1d84bf10339
veronzHub <veronica.coulombe@gmail.com>
2024-09-14 22:28:05 +0000

    sd"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        blombergF: ["var(--font-blomberg)"],
-      },
-    },
+    extend: {},
   },
   daisyui: {
     themes: [
