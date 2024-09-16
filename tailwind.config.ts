9cdb2336a9e4e7ff31bd7be82b8aa9d9a51ad1c9
veronzHub <veronica.coulombe@gmail.com>
2024-09-16 23:03:16 +0000

    removed comments

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,15 +19,15 @@ const config: Config = {
     themes: [
       {
         mytheme: {
-          primary: "#213445", // dar grey
-          secondary: "#3155d8", // blue
-          accent: "#d43fc4", // bright pink
-          neutral: "#5a5a5a", // light grey
-          "basle-100": "#8acfff", // light blue
-          info: "#3ABFF8", // reg
-          success: "#22C55E", // reg
-          warning: "#F59E08", // reg
-          error: "#EF4444", // reg
+          primary: "#213445", 
+          secondary: "#3155d8",
+          accent: "#d43fc4", 
+          neutral: "#5a5a5a", 
+          "base-100": "#8acfff", 
+          info: "#3ABFF8", 
+          success: "#22C55E", 
+          warning: "#F59E08", 
+          error: "#EF4444", 
         },
       },
     ],
