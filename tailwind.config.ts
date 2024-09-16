8c1106d1f576bfaafba299a38356b7c344cbd3fd
veronzHub <veronica.coulombe@gmail.com>
2024-09-16 22:27:40 +0000

    commented something out

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   content: [
     "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
