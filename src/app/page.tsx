f32e95455d81eacef3090dfda32d4d5e4f350ce0
veronzHub <veronica.coulombe@gmail.com>
2024-09-17 17:09:56 +0000

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ import Footer from "./components/footer";
 export default function Home() {
   const { scrollY } = useScroll();
   const [pageHeight, setPageHeight] = useState(0);
-  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const updateHeight = () => {
@@ -19,33 +18,16 @@ export default function Home() {
       }
     };
 
-    const checkIfMobile = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
     updateHeight();
-    checkIfMobile();
     window.addEventListener("resize", updateHeight);
-    window.addEventListener("resize", checkIfMobile);
 
-    return () => {
-      window.removeEventListener("resize", updateHeight);
-      window.removeEventListener("resize", checkIfMobile);
-    };
+    return () => window.removeEventListener("resize", updateHeight);
   }, []);
 
-  // Parallax effect for desktop (keep the same as before)
   const backgroundPositionY = useTransform(
     scrollY,
     [0, pageHeight],
     ["0%", "75%"]
-  );
-
-  // On mobile, manually adjust background position as it scrolls
-  const mobileBackgroundPositionY = useTransform(
-    scrollY,
-    [0, pageHeight],
-    ["0%", "30%"] // Adjust to how much you want the background to scroll on mobile
   );
 
   return (
@@ -53,11 +35,11 @@ export default function Home() {
       <motion.div
         className="stars-bg"
         style={{
-          backgroundPositionY: isMobile
-            ? mobileBackgroundPositionY
-            : backgroundPositionY,
+          backgroundPositionY,
         }}
-        initial={{ backgroundPositionY: "0%" }}
+        initial={{
+          backgroundPositionY: "0%",
+        }}
       ></motion.div>
 
       <Hero />
